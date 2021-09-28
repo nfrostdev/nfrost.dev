70967415a676137ca200082a36fb10038b65d737
Nick Frost <nfrost.dev@gmail.com>
2021-09-28 13:39:56 -0400

    prismic integration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Prismic from '@prismicio/client'
 import './assets/main.scss'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+  .use(router)
+app.config.globalProperties.$prismic = Prismic.client('https://nfrost.prismic.io/api/v2', {})
+app.mount('#app')
