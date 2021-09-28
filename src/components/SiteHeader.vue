a8b83a7e5bec8e0b5b575f2aba5a3a501d484a30
Nick Frost <nfrost.dev@gmail.com>
2021-09-28 11:45:50 -0400

    tailwind and some header styling

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,7 +1,15 @@
 <template>
   <header class="header">
-    <div>
-      <img src="@/assets/images/logo.svg" alt="NF Logo"/>
-    </div>
+    <img src="@/assets/images/logo.svg" alt="NF Logo" class="header__logo"/>
   </header>
 </template>
+
+<style lang="scss">
+.header {
+  @apply grid place-items-center text-nf-blue;
+
+  &__logo {
+    @apply h-12;
+  }
+}
+</style>
