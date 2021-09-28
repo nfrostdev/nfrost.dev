eab00dbc789d8918c643acd4012d91ca6cb09649
Nick Frost <nfrost.dev@gmail.com>
2021-09-28 16:17:58 -0400

    technology links

---
+++ src/components/TechnologyLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <a :href="technology.data.link.url"
+     target="_blank"
+     rel="noopener"
+     class="technology-link">{{ technology.data.name }}</a>
+</template>
+
+<script>
+export default {
+  props: {
+    technology: Object
+  }
+}
+</script>
+
+<style lang="scss">
+.technology-link {
+  @apply bg-white shadow-sm rounded-sm z-10 px-2 py-1 text-sm m-1 transition duration-200;
+
+  &:hover, &:focus {
+    @apply bg-nf-blue text-white;
+  }
+}
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="portfolio">
     <router-link v-for="project in projects"
-                 :key="project.uid"
+                 :key="project.id"
                  :to="{ name: 'Project', params: {uid: project.uid } }"
                  class="portfolio__link">
-      <div>{{ project.data.title[0].text }}</div>
+      <div class="portfolio__link__title" v-if="project.data.title">{{ project.data.title[0].text }}</div>
+      <div class="portfolio__link__clients">
+        <span v-for="client in project.data.clients" :key="client.uid">
+          {{ client.client.data.name }}
+        </span>
+      </div>
+      <div class="portfolio__link__technologies">
+        <technology-link v-for="technology in project.data.technologies"
+                         :key="technology.id"
+                         :technology="technology.technology"/>
+      </div>
     </router-link>
   </div>
 </template>
 
 <script>
 import Prismic from '@prismicio/client'
+import TechnologyLink from '@/components/TechnologyLink'
 
 export default {
+  components: { TechnologyLink },
   data () {
     return {
       projects: []
@@ -20,9 +32,11 @@ export default {
   },
   mounted () {
     this.$prismic.query(
-      Prismic.Predicates.at('document.type', 'project')
+      Prismic.Predicates.at('document.type', 'project'),
+      { fetchLinks: ['client.name', 'technology.name', 'technology.link'] }
     ).then(response => {
       this.projects = response.results.sort((a, b) => a.first_publication_date < b.first_publication_date ? 1 : -1)
+      console.log(this.projects[0])
     })
   }
 }
@@ -30,6 +44,18 @@ export default {
 
 <style lang="scss">
 .portfolio {
-  @apply grid place-items-center;
+  @apply grid place-items-center gap-6 p-6;
+
+  &__link {
+    @apply grid place-items-center text-center;
+
+    &__title {
+      @apply text-lg font-bold;
+    }
+
+    &__technologies {
+      @apply flex flex-wrap justify-center items-center;
+    }
+  }
 }
 </style>
