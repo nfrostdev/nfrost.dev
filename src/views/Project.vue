5755391d4000b01eed0f3ea960ec890073bc7596
Nick Frost <nfrost.dev@gmail.com>
2021-09-28 18:59:08 -0400

    individual project retrieval

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -3,3 +3,28 @@
     <h1>This is a Project page.</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      project: null
+    }
+  },
+  mounted () {
+    this.$prismic.getByUID('project', this.$route.params.uid.toString(),
+      {
+        fetchLinks: [
+          'client.name',
+          'contributor.name',
+          'contributor.link',
+          'language.name',
+          'technology.name'
+        ]
+      }).then(response => {
+      console.log(response.data)
+      this.project = response.data
+    })
+  }
+}
+</script>
