067e1b4a8ee2328cd07e86c7d50ae3f8030c49c9
Nick Frost <nfrost.dev@gmail.com>
2021-09-29 09:48:57 -0400

    display for projects

---
+++ src/components/ProjectBodyColumn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="project__body__column">
+    <h2 class="project__body__column__h2">{{ title }}</h2>
+    <div v-html="$prismicDom.RichText.asHtml(content)"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    content: Object
+  }
+}
+</script>
+
+<style lang="scss">
+.project__body__column {
+  &__h2 {
+    @apply text-xl font-bold mt-6;
+  }
+
+  p {
+    & + p {
+      @apply mt-4;
+    }
+  }
+}
+</style>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,35 +1,49 @@
 <template>
   <div class="project" v-if="loaded">
-    <div class="project__container">
-      <div v-html="$prismicDom.RichText.asHtml(project.title)"></div>
-      <project-attribute type="location"
-                         title="Location"
-                         :attributes="project.location"/>
-      <project-attribute type="client"
-                         title="Client"
-                         plural="Clients"
-                         :attributes="project.clients"/>
-      <project-attribute type="contributor"
-                         title="Contributor"
-                         plural="Contributors"
-                         :attributes="project.contributors"/>
-      <project-attribute type="language"
-                         title="Language"
-                         plural="Languages"
-                         :attributes="project.languages"/>
-      <project-attribute type="technology"
-                         title="Technology"
-                         plural="Technologies"
-                         :attributes="project.technologies"/>
+    <div class="project__info">
+      <project-image :image="project.image"/>
+      <div class="project__container">
+        <div v-html="$prismicDom.RichText.asHtml(project.title)"></div>
+        <project-attribute type="location"
+                           title="Location"
+                           :attributes="project.location"/>
+        <project-attribute type="client"
+                           title="Client"
+                           plural="Clients"
+                           :attributes="project.clients"/>
+        <project-attribute type="contributor"
+                           title="Contributor"
+                           plural="Contributors"
+                           :attributes="project.contributors"/>
+        <project-attribute type="language"
+                           title="Language"
+                           plural="Languages"
+                           :attributes="project.languages"/>
+        <project-attribute type="technology"
+                           title="Technology"
+                           plural="Technologies"
+                           :attributes="project.technologies"/>
+      </div>
+    </div>
+    <div class="project__body">
+      <project-body-column title="Details" :content="project.details"/>
+      <project-body-column title="Contributions" :content="project.contributions"/>
+      <project-body-column title="Obstacles" :content="project.obstacles"/>
     </div>
   </div>
 </template>
 
 <script>
 import ProjectAttribute from '@/components/ProjectAttribute'
+import ProjectImage from '@/components/ProjectImage'
+import ProjectBodyColumn from '@/components/ProjectBodyColumn'
 
 export default {
-  components: { ProjectAttribute },
+  components: {
+    ProjectBodyColumn,
+    ProjectImage,
+    ProjectAttribute
+  },
   data () {
     return {
       loaded: false,
@@ -59,9 +73,14 @@ export default {
 <style lang="scss">
 .project {
   @apply p-6 max-w-5xl mx-auto;
-}
 
-h1 {
-  @apply text-2xl font-bold;
+  h1 {
+    @apply text-xl font-bold mt-4;
+    @apply lg:text-3xl;
+  }
+
+  &__info {
+    @apply flex flex-col;
+  }
 }
 </style>
